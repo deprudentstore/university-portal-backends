bfd66b901247932b0dd1e9382e6b86f4012b367e
deprudentstore <rasheedlukmanayomide@gmail.com>
2026-08-05 00:08:41 +0100

    Fix CORS to allow multiple frontend origins

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,9 @@
 <?php
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => array_filter(array_map('trim', explode(',', env('FRONTEND_URLS', env('FRONTEND_URL', 'http://localhost:3000'))))),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
